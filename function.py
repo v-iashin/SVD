5713ec189a223a6dece7299f5f813e5574eb85ad
vdyashin <vdyashin@gmail.com>
2016-09-18 16:11:52 +0300

    Update function.py

--- function.py
+++ function.py
@@ -1,19 +1,19 @@
 def SVD(mat, initial_mat1, initial_mat2, learn_rate, iterations):
     ## reassigning values in order to keep code clean
-    # the matrix that we want to approximate
+    # the m by n matrix that we want to approximate
     A = mat
-    # two matrices from which we will start: B is n by k
+    # two matrices from which we will start: B is m by k
     B = initial_mat1
-    # C is m by k
+    # C is n by k
     C = initial_mat2
     # learning rate, or step of learning
     alpha = learn_rate
     # number of iterations
-    n = iterations
-    # A ~ b * c^t : the first approximation based on given initial matrices
+    N = iterations
+    # A ~ B * C^t : the first approximation based on given initial matrices
     A_app = np.dot(B, C.T)
-    # loop
-    for i in range(n):
+    # gradient descent
+    for i in range(N):
         # partial derivatives for matrices
         dLdB = np.dot((A_app - A), C)
         dLdC = np.dot((A_app - A).T, B)
