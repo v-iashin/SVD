55cf6f62fc635c0020e0a45c498ea44d4ead02e0
vdyashin <vdyashin@gmail.com>
2016-09-18 16:03:05 +0300

    Update function.py

--- function.py
+++ function.py
@@ -1,25 +1,26 @@
-def SVD(mat, initial_vec1, initial_vec2, learn_rate, iterations):
-    ## reassigning vectors in order to keep code clean
+def SVD(mat, initial_mat1, initial_mat2, learn_rate, iterations):
+    ## reassigning values in order to keep code clean
     # the matrix that we want to approximate
     A = mat
-    # two vectors from which we will start
-    b = initial_vec1
-    c = initial_vec2
+    # two matrices from which we will start: B is n by k
+    B = initial_mat1
+    # C is m by k
+    C = initial_mat2
     # learning rate, or step of learning
     alpha = learn_rate
     # number of iterations
     n = iterations
-    # A ~ b * c^t : the first approximation based on given initial vectors
-    A_app = np.dot(b, c.T)
+    # A ~ b * c^t : the first approximation based on given initial matrices
+    A_app = np.dot(B, C.T)
     # loop
     for i in range(n):
-        # partial derivatives for vectors
-        dLdb = np.dot((A_app - A), c)
-        dLdc = np.dot((A_app - A).T, b)
-        # updating vectors
-        c = c - alpha * dLdc
-        b = b - alpha * dLdb
+        # partial derivatives for matrices
+        dLdB = np.dot((A_app - A), C)
+        dLdC = np.dot((A_app - A).T, B)
+        # updating matrices
+        C = C - alpha * dLdC
+        B = B - alpha * dLdB
         # calculating approximated matrix
-        A_app = np.dot(b, c.T)
-    # returning two vectors that can be used for A approximation
-    return b, c, A_app
+        A_app = np.dot(B, C.T)
+    # returning two matrices that can be used for A approximation
+    return B, C, A_app
